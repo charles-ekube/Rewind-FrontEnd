8af896e41ab5afda1399c9e888a60b4f7785c06f
David Asamonye <davidasamonye@gmail.com>
2021-02-12 01:25:28 -0800

    Update Upload.js

--- src/pages/admin-dashboard/Upload.js
+++ src/pages/admin-dashboard/Upload.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { useForm } from "react-hook-form";
+
 import './Upload.css';
 import axios from "axios";
 import { Navbar, Side } from "../../components";
